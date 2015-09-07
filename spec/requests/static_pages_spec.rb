a8a3a8af7caf97c243b8038562fe510586bc9f07
Taiki <tyoshikawa1106@gmail.com>
2015-09-08 03:14:10 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,57 +2,34 @@ require "rails_helper"
 
 describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "タイトルのテスト" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
-    end
-
-    it "コンテンツのテスト" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "タイトルのテスト" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
-
-    it "コンテンツのテスト" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "タイトルのテスト" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
-
-    it "コンテンツのテスト" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "タイトルのテスト" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
-
-    it "コンテンツのテスト" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
