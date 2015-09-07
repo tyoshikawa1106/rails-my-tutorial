4f172d74d31ca26fe5249df9a887e50771474134
Taiki <tyoshikawa1106@gmail.com>
2015-09-07 23:06:52 +0900

    add Contact Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "タイトルのテスト" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
     it "コンテンツのテスト" do
@@ -19,7 +21,7 @@ describe "Static pages" do
 
     it "タイトルのテスト" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
     it "コンテンツのテスト" do
@@ -32,12 +34,25 @@ describe "Static pages" do
 
     it "タイトルのテスト" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
 
     it "コンテンツのテスト" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+  end
+
+  describe "Contact page" do
+
+    it "タイトルのテスト" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+
+    it "コンテンツのテスト" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
     end
   end
 end
