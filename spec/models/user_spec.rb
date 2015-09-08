65ff241bc42987e64b6b81a2203283f314ed32ca
Taiki <tyoshikawa1106@gmail.com>
2015-09-08 15:51:15 +0900

    update user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,6 +43,16 @@ describe User do
     end
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
