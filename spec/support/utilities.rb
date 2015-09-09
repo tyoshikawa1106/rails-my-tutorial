f8009ea038ed3fa7cf70e25ad2f2da73bf70dfee
Taiki <tyoshikawa1106@gmail.com>
2015-09-09 11:25:39 +0900

    update utilities.rb

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,14 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+def get_user()
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
