c9be916e70f33d2d71328d8a62f1f442894a0e06
Taiki <tyoshikawa1106@gmail.com>
2015-09-10 21:36:12 +0900

    add 404 error pages and add style

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  match '*path' => 'application#error404', via: :all
 end
