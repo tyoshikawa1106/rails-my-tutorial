dd553627f49dc2dc75070355baf6cd2debbfc8a9
Taiki <tyoshikawa1106@gmail.com>
2015-09-28 03:24:44 +0900

    update assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap-sf1/js/docs.js )
+Rails.application.config.assets.precompile += %w( bootstrap-sf1/dist/css/bootstrap.css )
+Rails.application.config.assets.precompile += %w( bootstrap-sf1/dist/css/docs.min.css )
